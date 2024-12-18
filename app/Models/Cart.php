2dcb34ae90c80da45261823a8405d7c2ca232682
kiokoshedy <shkmusembi@gmail.com>
2024-12-18 18:20:05 +0300

    init first

---
+++ app/Models/Cart.php
@@ -0,0 +1,18 @@
+// App\Models\Cart.php
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+    ];
+}
